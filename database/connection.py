b05a130a1330838150bff14a606cc50cf5a4728b
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2020-12-27 09:27:57 +0530

    using environmental variables in database credentials

--- database/connection.py
+++ database/connection.py
@@ -1,13 +1,19 @@
 import mysql.connector
+import os
+
+host = os.environ.get('host','localhost')
+user = os.environ.get('user')
+password = os.environ.get('password')
+database = 'ecommerce_db'
 
 class MySQLConnector:
 
     def __init__(self):
         self.mydb = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            passwd='gugu0902',
-            database='ecommerce_db'
+            host=host,
+            user=user,
+            passwd=password,
+            database=database
         )
 
         self.mycursor = self.mydb.cursor()
