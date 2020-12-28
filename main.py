585bb1852d0e09913960cb600fecd7d7ed8c4f65
Gustavo Azevedo Naldoni <gunaldoni@gmail.com>
2020-12-28 13:16:28 -0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from interface.initial_messages import show_welcome_message, show_welcome_options
+from interface.products_messages import show_all_products
+
+show_welcome_message("Naldoni's")
+show_welcome_options()
+
+# show_all_products()
