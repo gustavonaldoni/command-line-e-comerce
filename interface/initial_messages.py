474ff8af0cfb2e25b075db9b374682b282e38ee3
Gustavo Azevedo Naldoni <gunaldoni@gmail.com>
2020-12-28 13:12:32 -0300

    Add interface/ folder to contain all the 'front-end' stuff

---
+++ interface/initial_messages.py
@@ -0,0 +1,12 @@
+def show_welcome_message(store_name):
+    print()
+    print(f'==== Welcome to {store_name} ====')
+    print()
+
+def show_welcome_options():
+    print("""-- Main Menu --
+
+[1] Register
+[2] Login
+[3] Exit program
+    """)
