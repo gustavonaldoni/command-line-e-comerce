5d2ddb8dfa613513a2467da46efb0da1b83f67f4
Gustavo Azevedo Naldoni <gunaldoni@gmail.com>
2020-12-29 14:34:14 -0300

    Delete modules/ and register_and_login/ folder

--- modules/accounts.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-
-from database.operations import AccountHandler
-
-
-def register(first_name,last_name,email, password):
-    handler = AccountHandler()
-    password_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-    handler.add_user(first_name,last_name,email,password_hash)
