e8c21d543360f48ae9380a3d62d1caf64b24ca25
Gustavo Azevedo Naldoni <gunaldoni@gmail.com>
2020-12-29 14:24:18 -0300

    Add ask_user_for_welcome_option() function

--- interface/initial_messages.py
+++ interface/initial_messages.py
@@ -1,12 +1,17 @@
 def show_welcome_message(store_name):
     print()
     print(f'==== Welcome to {store_name} ====')
-    print()
 
 def show_welcome_options():
+    print()
     print("""-- Main Menu --
 
 [1] Register
 [2] Login
-[3] Exit program
-    """)
+[3] Exit program""")
+
+def ask_user_for_welcome_option():
+    print()
+    user_choice = int(input('Your choice: '))
+
+    return user_choice
