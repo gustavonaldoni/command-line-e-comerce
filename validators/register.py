ed90a23e8cabc0b67cef1ae1cbbe24b4d0a21c1d
Gustavo Azevedo Naldoni <gunaldoni@gmail.com>
2020-12-30 11:33:34 -0300

    Add validate_password() method and unittest it

--- validators/register.py
+++ validators/register.py
@@ -1,4 +1,5 @@
 import re 
+import string
 
 
 class RegisterValidator:
@@ -12,3 +13,33 @@ class RegisterValidator:
             return False
        
         return True
+
+    def __check_if_at_least_one_character_matches(self, string1, string2):
+        for character in string1:
+            if character in string2:
+                return True
+
+        return False
+
+    def validate_password(self, password):
+        
+        requirements = (
+            string.ascii_uppercase,
+            string.ascii_lowercase,
+            string.digits,
+            string.punctuation
+        )
+        
+        needed_requirements = 5
+
+        requirements_counter = 0
+        
+        if 5 <= len(password) <= 20:
+            requirements_counter += 1
+
+        for requirement in requirements:
+            if self.__check_if_at_least_one_character_matches(password, requirement):
+                requirements_counter += 1
+    
+        return requirements_counter == needed_requirements  
+            
--- validators/test_register.py
+++ validators/test_register.py
@@ -18,3 +18,20 @@ class TestRegisterValidator:
 
         for email, result in examples.items():
             assert self.validator.validate_email(email) == result, print(f'ERROR ON -> {email}')
+
+    def test_validate_password(self):
+
+        examples = {
+            'Aaaa9*' : True,
+            'Aggsgf4536*(' : True,
+            'huHu489398***' : True,
+            'aaa' : False,
+            'Ab6*' : False,
+            'fdhskagdfgkjhdsaA' : False,
+            'passworD' : False,
+            'passworD123' : False,
+            '123456' : False
+        }
+
+        for password, result in examples.items():
+            assert self.validator.validate_password(password) == result, print(f'ERROR ON -> {password}')
