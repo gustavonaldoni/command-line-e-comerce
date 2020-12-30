2abfb2ddde528b1c111ae8881c1da7a07ca42b36
Gustavo Azevedo Naldoni <gunaldoni@gmail.com>
2020-12-30 12:10:49 -0300

    Add another messages to be shown

--- interface/register_messages.py
+++ interface/register_messages.py
@@ -9,3 +9,18 @@ def ask_user_for_register():
     user_password = input('Password: ')
 
     return (user_first_name, user_last_name, user_email, user_password)
+
+def show_success_registration_message():
+    print()
+    print('SUCCESS! - Your user account has been registered without errors.')
+
+def show_invalid_email_message(user_email):
+    print(f'Your email "{user_email}" is invalid.')
+
+def show_invalid_password_message(user_password):
+    print(f'Your password "{user_password}" is invalid.')
+
+def show_error_registration_message():
+    print()
+    print('Oh no ... an error has occurred during your registration process')
+    print('Please contact the support for more informations')
