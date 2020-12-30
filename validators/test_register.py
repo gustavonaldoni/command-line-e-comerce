77e012a6cff0925d7df88af3485bcc25e39a6e6a
Gustavo Azevedo Naldoni <gunaldoni@gmail.com>
2020-12-30 10:46:02 -0300

    Add unittest to validate_email() method

---
+++ validators/test_register.py
@@ -0,0 +1,20 @@
+from register import RegisterValidator
+
+
+class TestRegisterValidator:
+
+    validator = RegisterValidator()
+
+    def test_validate_email(self):
+        examples = {
+            'a@a.com' : True,
+            'aaaaa@gmail.com' : True,
+            '1233@gmail.com' : True,
+            'gutgfuy12345@hotmail.com' : True,
+            'aaaaa@123.com' : True,
+            'aaaaa@gmail.com.br' : True,
+            'trewtrew' : False
+        }
+
+        for email, result in examples.items():
+            assert self.validator.validate_email(email) == result, print(f'ERROR ON -> {email}')
