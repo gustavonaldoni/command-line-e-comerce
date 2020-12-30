2926712bc4ec58f861f966c184cac7f46b4320da
Gustavo Azevedo Naldoni <gunaldoni@gmail.com>
2020-12-30 10:45:26 -0300

    Create RegisterValidator class in order to validate information given by user when registering

--- validators/register.py
+++ validators/register.py
@@ -1,2 +1,14 @@
+import re 
+
+
 class RegisterValidator:
-    pass
+    
+    def validate_email(self, email):
+        pattern = re.compile(r'\w+@\w+\.\w+')
+
+        result = pattern.search(email)
+
+        if result is None:
+            return False
+       
+        return True
