3aaf1372f8fa7b634ba56ce4c3c5420ed6b5c352
Gustavo Azevedo Naldoni <gunaldoni@gmail.com>
2020-12-31 13:58:04 -0300

    Add show_success_login_message() function

--- interface/login_messages.py
+++ interface/login_messages.py
@@ -11,3 +11,9 @@ def ask_user_to_login():
 def show_invalid_email_message(user_email):
     print()
     print(f'Your email "{user_email}" does not exist.')
+
+def show_success_login_message(user_first_name, user_last_name):
+    full_name = f'{user_first_name} {user_last_name}'
+
+    print()
+    print(f'Hi, {full_name}! You are logged in.')
