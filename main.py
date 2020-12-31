c6216430daaabd1c5a620ad0b52d0923c2b60083
Gustavo Azevedo Naldoni <gunaldoni@gmail.com>
2020-12-31 13:45:07 -0300

    Add the Login functionality properly

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from interface.initial_messages import show_welcome_message, show_welcome_option
 from interface.register_messages import ask_user_for_register, show_success_registration_message, show_invalid_email_message, show_invalid_password_message, show_error_registration_message
 from interface.products_messages import show_all_products
 from interface.terminal_cleaner import clean_terminal
-from database.operations import RegisterOperator, AccountHandler
+from interface.login_messages import ask_user_to_login, show_invalid_email_message
+from database.operations import RegisterOperator, AccountHandler, LoginOperator
 from validators.register import RegisterValidator
+from general_classes.user import User
 
 def main():
 
@@ -46,7 +48,39 @@ def main():
             clean_terminal(3)
 
         elif user_option == 2:
-            pass
+            clean_terminal(0.5)
+
+            user_email, user_password = ask_user_to_login()
+
+            login_operator = LoginOperator()
+
+            does_email_exist = login_operator.check_if_email_exists(user_email)
+
+            if not does_email_exist:
+                show_invalid_email_message(user_email)
+                
+            else:
+                user_id, hashed_password = login_operator.select_id_and_hashed_password_by_email(user_email)[0]
+                
+                is_password_valid = login_operator.check_password(user_password, hashed_password)
+
+                if is_password_valid:
+                    account_handler = AccountHandler()
+
+                    all_account_information = account_handler.select_all_user_information(user_id)[0]
+
+                    user_first_name = all_account_information[1]
+                    user_last_name = all_account_information[2]
+                    user_email = all_account_information[3]
+                    user_creation_datetime = all_account_information[5]
+
+                    user = User(user_id, user_first_name, user_last_name, user_email, user_creation_datetime)
+
+                else:
+                    print('INVALID PASSWORD')
+
+            clean_terminal(3)
+
         elif user_option == 3:
             pass
        
