bc07af35b489042f1e2b722ff0f998e7228ea795
Gustavo Azevedo Naldoni <gunaldoni@gmail.com>
2020-12-31 13:42:41 -0300

    Add id and account_creation_date attributes to User class

--- general_classes/user.py
+++ general_classes/user.py
@@ -1,6 +1,8 @@
 class User:
 
-    def __init__(self, first_name, last_name, email):
+    def __init__(self, id, first_name, last_name, email, account_creation_date):
+        self.id = id
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
+        self.account_creation_date = account_creation_date
